853a6ad3a38b7ade34ab4ecb812b6b29bff810e9
Avin Abraham <avin@nwk--208-39.nwk.ruw.rutgers.edu>
2016-10-23 03:18:11 -0400

    message

--- mongodb_access.py
+++ mongodb_access.py
@@ -1,48 +1,63 @@
-from flask import Flask 
+#from flask import Flask 
 from flask_pymongo import PyMongo 
 
-app = Flask(__name__)
+#app = Flask(__name__)
 
 app.config['MONGO_DBNAME'] = 'user_schedules_db'
 app.config['MONGO_URI'] = 'mongodb://yerdman:hackru2016@ds063946.mlab.com:63946/user_schedules_db'
 
 mongo = PyMongo(app) 
 
-#@app.route('/add_user')
+@app.route('/add_user')
 def add_user(name,number,event):
 	#name = 'Avin'
 	#number = '9873049872'
 	#event = [1,2,3]
-	events = [event]
+	#events = [event]
 	user = mongo.db.user_schedules_db
 	user.insert({'name' : name, 'number' : number,'events' : events})
 	return 'Added User!'
 
-#@app.route('/add_event')
+@app.route('/add_event')
 def add_event(number,event):
 	#number = '9873049872'
 	#event = [4,5,6]
 	user = mongo.db.user_schedules_db
-	current = user.find_one({'name' : name})
+	current = user.find_one({'number' : number})
 	current['events'].append(event)
 	user.save(current)
 	return 'Added Messages!'
 
 
-#@app.route('/get_events')
+@app.route('/get_events')
 def get_events(number):
 	#number = '9873049872'
 	user = mongo.db.user_schedules_db
-	current = user.find_one({'number' : number})
-	return(str(current['events']))
+	try:
+		current = user.find_one({'number' : number})
+		return(str(current['events']))
+	except Exception as e:
+		return("ERROR")
+	else:
+		pass
+	finally:
+		pass
 
-#@app.route('/get_name')
+@app.route('/get_name')
 def get_name(number):
 	#number = '9873049872'
 	user = mongo.db.user_schedules_db
-	current = user.find_one({'number' : number})
-	return(str(current['name']))
+	try:
+		current = user.find_one({'number' : number})
+		return(str(current['name']))
+	except Exception as e:
+		return("ERROR")
+	else:
+		pass
+	finally:
+		pass
+	
 
-#if __name__ == '__main__':
-#	app.run(debug=True)
+if __name__ == '__main__':
+	app.run(debug=True)
   
