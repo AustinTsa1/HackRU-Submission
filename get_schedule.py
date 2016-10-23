ebfd74d9974371e844cead8af9f456d72864684d
avinr193 <aja193@scarletmail.rutgers.edu>
2016-10-23 04:58:08 -0400

    Add files via upload

--- get_schedule.py
+++ get_schedule.py
@@ -1,5 +1,5 @@
 def getSchedule(messages):
-#messages = [["HACKRU", "COLLEGEAVE", "10:30am", "5:30pm", "10/4/16","30"],["HACKRU", "COLLEGEAVE", "10:30am", "5:30pm", "10/4/16","30"]]
+	#messages = [["HACKRU", "COLLEGEAVE", "10:30am", "5:30pm", "10/4/16","30"],["HACKRU", "COLLEGEAVE", "10:30am", "5:30pm", "10/4/16","30"]]
 	names = [""]
 	for i, item in enumerate(messages):
 		event = messages[i][0]
@@ -8,12 +8,12 @@ def getSchedule(messages):
 		end_time = messages[i][3]
 		date = messages[i][4]
 		notify = messages[i][5]
-		names.append(str(i) + ': ["HACKRU", "COLLEGEAVE", "10:30am", "5:30pm", "30"]' + 'You have ' + event + ' at ' + location + '. It starts at ' + start_time + ' and will end at ' + end_time + ' on ' + date + '. It is ' + notify + ' minutes before the event.\n')
+		names.append(str(i) + ':' + 'You have ' + event + ' at ' + location + '. It starts at ' + start_time + ' and will end at ' + end_time + ' on ' + date + '. It is ' + notify + ' minutes before the event.\n')
 	
 	z = ""
 
 	for x, item in enumerate(names):
 		z += names[x]
 
-#print(z)
+	#print(z)
 	return(z)
