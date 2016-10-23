b34deb3cd4b54768c0a2a8de7184285840bda5ae
eminkin97 <eminkin99@gmail.com>
2016-10-23 00:13:09 -0400

    updates to main.py.

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
 from flask import Flask, request, render_template
+import twilio.twim1
+
 app = Flask(__name__)
 
 @app.route("/", methods = ['GET', 'POST'])
 def checklist():
-	return render_template("a.html")
+	number = request.form['From']
+	message_body = request.form['Body']
+
+	return render_template("login.html")
+
+@app.route("/<user>")
+def index(user=None):
+	return render_template("user.html", user=user)
 
 if __name__ = "__main__":
 	app.run()
