b9b6117481ba40e8962e8d20bda731fe2f423aa4
eminkin97 <eminkin99@gmail.com>
2016-10-23 01:00:35 -0400

    added return message to user's phone

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from flask import Flask, request, render_template
 import twilio.twim1
+#import getSchedule.py ?
+#import string_manipulation.py ?
 
 app = Flask(__name__)
 
@@ -7,6 +9,17 @@ app = Flask(__name__)
 def checklist():
 	number = request.form['From']
 	message_body = request.form['Body']
+	//INSERT CODE TO SPLIT UP MESSAGE BODY
+
+	//PUT RESULTING ARRAY IN A MONGODB DATABASE
+
+	//RESPONSE TO PHONE
+	message = getSchedule()
+
+	resp = twilio.twiml.Response()
+	resp.message(message)
+
+	return str(resp)
 
 	return render_template("login.html")
 
